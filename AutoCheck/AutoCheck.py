0a9739ec5037f4ba5fe23d49a79922cf005366d3
Space-Luis <57285437+Space-Luis@users.noreply.github.com>
2020-11-14 00:36:41 +0900

    Add files via upload

---
+++ AutoCheck/AutoCheck.py
@@ -0,0 +1,49 @@
+import hcskr
+
+data = hcskr.selfcheck("한민석","030428","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("김민섭","030428","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("임재모","030514","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("장성민","030128","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("성현우","031208","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("민정규","030711","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("주연후","031006","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("양은준","030109","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("유지담","031112","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("박건규","030520","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("문가현","030103","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("김해민","031121","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("신나리","030329","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("김재민","031230","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("정수현","030413","서울","둔촌고","고등학교")
+print(data['message'])
+
+data = hcskr.selfcheck("김동현","031206","서울","둔촌고","고등학교")
+print(data['message'])
