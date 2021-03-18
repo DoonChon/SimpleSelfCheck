3e3ab34bf8892af3f4c70e502405182bd7cb4368
Space-Luis <57285437+Space-Luis@users.noreply.github.com>
2021-03-18 12:01:33 +0900

    Update AutoCheck.py

--- AutoCheck/AutoCheck.py
+++ AutoCheck/AutoCheck.py
@@ -1,4 +1,6 @@
 import hcskr
 
+# hcskr.selfcheck("이름","생년월일","지역명","학교명","학교구분","비밀번호")
+
 data = hcskr.selfcheck("한민석","030428","서울","둔촌고","고등학교","7530")
-print(data['message'])
+print(data['message']) //성공적으로 완료됬음을 표시
